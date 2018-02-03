18bcb8000785299d569d84aa4f057281bbb6a65b
1alexamartinez <1alexamartinez@gmail.com>
2018-02-03 17:06:47 -0500

    first commit

---
+++ bamazonCustomer.js
@@ -0,0 +1,12 @@
+//I have created a table in MySql called Products that hold information about the product_name, department_name, price, and stock_quantity.
+
+// I will populate the table with dummy information provided by myself
+
+//1.The app will first ask the user what they would like to purchase 2. then ask them about the quantity of the item.
+
+//the app will keep track of how many items it has "in stock". 
+
+//if product is out of stock then app will notify the user.
+
+//the app will reflect what is in the bamazon "stock".
+
